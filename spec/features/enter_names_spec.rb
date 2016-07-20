43e96623b6db9d356863002e1a1dbf6b17b28a8d
acsauk <acsauk@gmail.com>
2016-07-20 12:07:47 +0100

    completes exercise #20 - displaying player 2 HP

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -11,3 +11,20 @@ feature "entering names" do
 
   end
 end
+
+# As Player 1,
+# So I can see how close I am to winning
+# I want to see Player 2's Hit Points
+
+feature "Hit points" do
+  scenario "displaying player 2" do
+    visit "/"
+
+    fill_in :first_player, with: "Alex"
+    fill_in :second_player, with: "Zeeshan"
+    click_button "submit"
+
+    expect(page).to have_content("Zeeshan HP: 10")
+
+  end
+end
